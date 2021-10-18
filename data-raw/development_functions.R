c8efd84027d0791fecbecc7efbe9b0b7aa0663c1
Saiem Gilani <saiem.gilani@gmail.com>
2021-10-18 12:59:54 -0400

    moved to data-raw, probably no longer necessary shortly

---
+++ data-raw/development_functions.R
@@ -0,0 +1,30 @@
+## the functions here still need work to account for the variation
+## in how the NWHL/PHF does weird data structure stuff
+
+shot_data <- function(data) {
+
+  score <- data[[max(length(data)) - 2]]
+  shot <- data[[max(length(data)) - 1]]
+
+  score <- score %>%
+    clean_names() %>%
+    rename("team" = "scoring",
+           "first_scoring" = "x1st",
+           "second_scoring" = "x2nd",
+           "third_scoring" = "x3rd",
+           "total_scoring" = "t")
+
+  shot <- shot %>%
+    clean_names() %>%
+    rename("team" = "shots",
+           "first_shots" = "x1st",
+           "second_shots" = "x2nd",
+           "third_shots" = "x3rd",
+           "total_shots" = "t")
+
+  shot %>%
+    left_join(score, by = c("team"))
+
+}
+
+
