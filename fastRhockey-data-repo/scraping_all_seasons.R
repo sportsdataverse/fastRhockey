63b4cf654d6976722778b56d61f927221a64767a
Benjamin Howell <ben.howell@utexas.edu>
2021-10-31 15:14:39 -0500

    Create scraping_all_seasons.R

---
+++ fastRhockey-data-repo/scraping_all_seasons.R
@@ -0,0 +1,43 @@
+library(fastRhockey)
+library(tidyverse)
+
+# phf_schedule(season = 2021)
+
+lst <- list()
+
+for (k in 2017:2021) {
+  
+  current <- phf_schedule(season = k) %>%
+    dplyr::select(game_id)
+  
+  lst[[k]] <- current
+  
+  print(k)
+  
+}
+
+games <- dplyr::bind_rows(lst) %>%
+  dplyr::mutate(order = row_number())
+
+lst2 <- list()
+mx <- max(games$order, na.rm = TRUE)
+
+for (z in 1:mx) {
+  
+  gm <- games %>%
+    dplyr::filter(order == z) %>%
+    dplyr::select(game_id) %>%
+    dplyr::pull(game_id)
+  
+  df <- load_pbp(game_id = gm)
+  
+  lst2[[z]] <- df
+  
+  print(z)
+  
+}
+
+fast_pbp <- dplyr::bind_rows(lst2)
+
+  
+  
