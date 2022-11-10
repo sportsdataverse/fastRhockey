18520554489696379063543cbdbae2436ca0b815
Benjamin Howell <ben.howell@utexas.edu>
2022-11-10 00:12:13 -0600

    skater/goalie stats + boxscore data and we're finished!

---
+++ data-raw/raw_data/pwhpa_tests/pwhpa_boxscore.R
@@ -0,0 +1,4 @@
+
+pwhpa_boxscore <- function(game_id) {
+  # https://stats.pwhpa.com/event/scotiabank-vs-sonnet/  
+}
---
+++ data-raw/raw_data/pwhpa_tests/pwhpa_stats.R
@@ -0,0 +1,9 @@
+
+pwhpa_stats <- function(position = "skaters") {
+  
+  # https://stats.pwhpa.com/list/scoring-leaders/
+  # https://stats.pwhpa.com/list/goaltending/
+  
+  
+  
+}
