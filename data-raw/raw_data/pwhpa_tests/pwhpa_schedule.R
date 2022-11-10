12236ffc54bf57ef816ab24dd89a98fe3372319e
Benjamin Howell <ben.howell@utexas.edu>
2022-11-09 22:04:21 -0600

    pwhpa schedule function

--- data-raw/raw_data/pwhpa_tests/pwhpa_schedule.R
+++ data-raw/raw_data/pwhpa_tests/pwhpa_schedule.R
@@ -2,6 +2,51 @@ library(tidyverse)
 library(rvest)
 library(httr)
 
+process_game_info <- function(game_info) {
+
+  date <- substr(html_text(game_info[[1]]), 0, 19)
+  date_text <- substr(html_text(game_info[[1]]), 20, nchar(html_text(game_info[[1]])))
+
+  home_tm <- stringr::str_trim(html_text(game_info[[2]]))
+  away_tm <- stringr::str_trim(html_text(game_info[[4]]))
+
+  if (grepl("pm", html_text(game_info[[3]]))) {
+
+    home_scr <- NA
+    away_scr <- NA
+
+  } else {
+
+    home_scr <- stringr::str_split(html_text(game_info[[3]]), " - ", n = 2)[[1]][1]
+    away_scr <- stringr::str_split(html_text(game_info[[3]]), " - ", n = 2)[[1]][2]
+
+  }
+
+  location <- html_text(game_info[[5]])
+
+  game_link <- game_info[[6]] %>% html_node("a") %>% html_attr("href")
+
+  game_id <- stringr::str_remove(stringr::str_remove(game_link, "https://stats.pwhpa.com/event/"), '/')
+
+  winner <- ifelse(home_scr > away_scr, home_tm, ifelse(away_scr > home_scr, away_tm, "Tie"))
+
+  game_df <- data.frame(
+    game_date = c(date),
+    game_id = c(game_id),
+    date_text = c(date_text),
+    home_team = c(home_tm),
+    home_score = c(home_scr),
+    away_team = c(away_tm),
+    away_score = c(away_scr),
+    winner = c(winner),
+    location = c(location),
+    game_link = c(game_link)
+  )
+
+  return(game_df)
+
+}
+
 pwhpa_schedule <- function() {
 
   full_url <- "https://stats.pwhpa.com/calendar/secret-dream-gap-tour-schedule/"
@@ -18,44 +63,24 @@ pwhpa_schedule <- function() {
   g <- lst[1] %>%
     rvest::html_nodes("tr")
 
-  n <- g[[1]] %>%
-    rvest::html_nodes("td")
+  lst <- list()
 
-  date <- substr(html_text(n[[1]]), 0, 19)
-  date_text <- substr(html_text(n[[1]]), 20, nchar(html_text(n[[1]])))
+  for (id in seq(1:length(g))) {
 
-  home_tm <- stringr::str_trim(html_text(n[[2]]))
-  away_tm <- stringr::str_trim(html_text(n[[4]]))
-  home_scr <- stringr::str_split(html_text(n[[3]]), " - ", n = 2)[[1]][1]
-  away_scr <- stringr::str_split(html_text(n[[3]]), " - ", n = 2)[[1]][2]
+    game_info <- g[[id]] %>%
+      rvest::html_nodes("td")
 
-  location <- html_text(n[[5]])
 
-  game_link <- n[[6]] %>% html_node("a") %>% html_attr("href")
+    game_df <- process_game_info(game_info = game_info)
 
-  game_id <- stringr::str_remove(stringr::str_remove(game_link, "https://stats.pwhpa.com/event/"), '/')
+    lst[[id]] <- game_df
 
-  winner <- ifelse(home_scr > away_scr, home_tm, ifelse(away_scr > home_scr, away_tm, "Tie"))
+  }
 
-  data.frame(
-    game_date = c(date),
-    game_id = c(game_id),
-    date_text = c(date_text),
-    home_team = c(home_tm),
-    home_score = c(home_scr),
-    away_team = c(away_tm),
-    away_score = c(away_scr),
-    winner = c(winner),
-    location = c(location),
-    game_link = c(game_link)
-  )
+  schedule_df <- dplyr::bind_rows(lst)
 
-  # print.AsIs(g[1])
-
-  # for (y in g[[1]]) {
-  #
-  #   print(y)
-  #
-  # }
+  return(schedule_df)
 
 }
+
+df <- pwhpa_schedule()
